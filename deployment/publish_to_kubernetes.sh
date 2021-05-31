3ef379d9d13748b3773ede06c2248f193d6e0adb
Adeola <ojoadeolagabriel@gmail.com>
2021-05-31 08:00:14 +0100

    changes

--- deployment/publish_to_kubernetes.sh
+++ deployment/publish_to_kubernetes.sh
@@ -5,5 +5,5 @@ set -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 kubectl delete namespace py-ns || :
-kubectl apply -f "${DIR}/apply.yml"
+kubectl apply -f "${DIR}/service.yml"
 kubectl apply -f "${DIR}/nginx/nginx.yml"
